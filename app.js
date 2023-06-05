2b76d70a0b998f8b1d903db92d0f45ff2a88d2bf
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-05 17:27:42 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+const connectDB = require("./config/db")
+const student_router = require("./routes/student")
+const admin_router = require("./routes/admin")
+const env = require("./config/dotenv")
+require('express-async-errors');
+
+
+app.use(express.json())
+app.use('/api/student', student_router)
+app.use('/api/admin', admin_router)
+
+app.listen(env.PORT, async () => {
+    console.log("Server started on PORT", env.PORT)
+    if (await connectDB()) {
+        console.log("Connected to DB")
+    }
+    else {
+        console.log("Internal DB Error");
+    }
+})
