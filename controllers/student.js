cde66100918ad47adfc701ee2d990346e8c3db5b
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-06 12:43:25 +0530

    Update student.js

--- controllers/student.js
+++ controllers/student.js
@@ -6,9 +6,17 @@ const { encryptPassword, checkPassword } = require("../utils/encrypt-decrypt")
 
 const signUp = async (req, res) => {
     const { register_number, name, password, courses } = req.body;
+    if (!register_number || !name || !password || !courses) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
     course_list = String(courses).split(",")
-    console.log(course_list)
-    password_hash = await encryptPassword(password)
+    password_hash = encryptPassword(password)
+    const courses_check = await Course.find({ courseName: { $in: course_list } })
+    if (courses_check.length !== course_list.length) {
+        res.status(400).json({ message: "Invalid Course" })
+        return;
+    }
     try {
 
         const response = await Student.create({
@@ -26,13 +34,19 @@ const signUp = async (req, res) => {
 
 const login = async (req, res) => {
     const { register_number, password } = req.body;
-    const student = Student.find({ register_number })
+    if (!register_number || !password) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
+    const student = await Student.findOne({ regno: register_number })
+
     if (!student) {
         res.status(401).json({ message: "Invalid Username" })
     }
     else {
-        if (checkPassword(student.password, password)) {
-            const token = await jwt.sign({ register_number, password }, process.env.JWT_SECRET, { expiresIn: '1h' })
+        const flag = checkPassword(String(student.password), String(password))
+        if (flag) {
+            const token = jwt.sign({ register_number, password }, process.env.JWT_SECRET, { expiresIn: '1h' })
             res.status(200).json({ token })
         }
         else {
@@ -42,13 +56,22 @@ const login = async (req, res) => {
 }
 
 const registerCourse = async (req, res) => {
-    const { course_id } = req.body;
-    const course = await Course.findById(course_id)
+    const { course_id, register_number } = req.body;
+    if (!course_id || !register_number) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
+    const course = await Course.findOne({ courseCode: course_id })
     if (!course) {
         res.status(401).json({ message: "Invalid Course ID" })
+        return;
     }
     else {
-        const student = await Student.findById(req.register_number)
+        const student = await Student.findOne({ regno: register_number })
+        if (!student) {
+            res.status(401).json({ message: "Invalid Register Number" })
+            return;
+        }
         if (student.courses.includes(course_id)) {
             res.status(401).json({ message: "Course already registered" })
         }
@@ -66,7 +89,8 @@ const listCourses = async (req, res) => {
 }
 
 const viewRegisteredCourses = async (req, res) => {
-    const student = await Student.findById(req.student.register_number).populate('courses')  // populate() is used to get the details of the courses from the course_id
+    const { register_number } = req.body;
+    const student = await Student.findOne({ regno: register_number }).populate('courses')  // populate() is used to get the details of the courses from the course_id
     res.status(200).json({ courses: student.courses })
 }
 
