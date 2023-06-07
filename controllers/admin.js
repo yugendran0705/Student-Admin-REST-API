ee25cb865237a0ddbbf70cd346898694431a0fd7
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-07 22:51:33 +0530

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,114 +6,100 @@ const Admin = require('../models/Admin');
 const { JWT_SECRET_KEY_ADMIN } = require('../config/dotenv');
 
 const signup = async (req, res) => {
+    const { id, password, role, name } = req.body;
+    if (!id || !password || !role || !name) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
     try {
-        const { id, password, role, name } = req.body;
-        if (!id || !password || !role || !name) {
-            res.status(400).json({ message: "Invalid Input" })
-            return;
-        }
-        try {
-            password_hash = encryptPassword(password)
-            const response = await Admin.create({
-                admin_id: id,
-                password: password_hash,
-                role: role,
-                name: name
-            });
-            res.status(201).json({ message: "Admin Created" })
-        }
-        catch (error) {
-            res.status(409).json({ message: "Admin already exists" });
-        }
+        password_hash = encryptPassword(password)
+        const response = await Admin.create({
+            admin_id: id,
+            password: password_hash,
+            role: role,
+            name: name
+        });
+        res.status(201).json({ message: "Admin Created" })
     }
     catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
+        res.status(409).json({ message: "Admin already exists" });
     }
 }
 
 const login = async (req, res) => {
+    const { id, password, role } = req.body
+    if (!id || !password || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
     try {
-        const { id, password, role } = req.body
-        if (!id || !password || !role) {
-            res.status(400).json({ message: "Invalid Input" })
-            return;
-        }
         const admin = await Admin.findOne({ admin_id: id });
         if (!admin || admin.role !== role) {
             res.status(404).json({ message: "Admin not found" })
             return;
         }
-        const flag = checkPassword(String(admin.password), String(password))
-        if (flag) {
-            const token = jwt.sign({ id, role }, JWT_SECRET_KEY_ADMIN, { expiresIn: '1h' });
-            res.status(200).json({ message: "Admin Logged In", token })
-        }
-        else {
-            res.status(401).json({ message: "Invalid Password" })
-        }
     }
     catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
+        res.status(404).json({ message: "Admin not found" })
+        return;
+    }
+    const flag = checkPassword(String(admin.password), String(password))
+    if (flag) {
+        const token = jwt.sign({ id, role }, JWT_SECRET_KEY_ADMIN, { expiresIn: '1h' });
+        res.status(200).json({ message: "Admin Logged In", token })
+    }
+    else {
+        res.status(401).json({ message: "Invalid Password" })
     }
 }
 
 const createCourse = async (req, res) => {
-    try {
-        const { courseName, courseCode, capacity, role } = req.body;
-        if (!courseName || !courseCode || !capacity || !role) {
-            res.status(400).json({ message: "Invalid Input" })
-            return;
-        }
-        if (role !== "admin") {
-            res.status(401).json({ message: "Unauthorized" })
-            return;
-        }
-        else {
-            try {
-                const response = await Course.create({
-                    courseName,
-                    courseCode,
-                    capacity: parseInt(capacity),
-                });
-                res.status(200).json({ message: "Course Created" })
-            }
-            catch (error) {
-                res.status(409).json({ message: "Course already exists" });
-            }
-        }
+    const { courseName, courseCode, capacity, role } = req.body;
+    if (!courseName || !courseCode || !capacity || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
     }
-    catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
+    if (role !== "admin") {
+        res.status(401).json({ message: "Unauthorized" })
+        return;
+    }
+    else {
+        try {
+            const response = await Course.create({
+                courseName,
+                courseCode,
+                capacity: parseInt(capacity),
+            });
+            res.status(200).json({ message: "Course Created" })
+        }
+        catch (error) {
+            res.status(409).json({ message: "Course already exists" });
+        }
     }
 }
 
 const deleteCourse = async (req, res) => {
-    try {
-        const { courseCode, courseName, role } = req.body;
-        if (!courseCode || !courseName || !role) {
-            res.status(400).json({ message: "Invalid Input" })
-            return;
-        }
-        if (role !== "admin") {
-            res.status(401).json({ message: "Unauthorized" })
-            return;
-        }
-        else {
-            try {
-                const course = await Course.findOne({ courseCode: courseCode });
-                if (!course) {
-                    res.status(404).json({ message: "Course not found" })
-                }
-                await Course.deleteOne({ courseCode: req.body.courseCode, courseName: req.body.courseName });
-                res.status(200).json({ message: "Course Deleted" })
-            }
-            catch (error) {
-                res.status(500).json({ message: "Internal Server Error" })
-            }
-        }
+    const { courseCode, courseName, role } = req.body;
+    if (!courseCode || !courseName || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
     }
-    catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
+    if (role !== "admin") {
+        res.status(401).json({ message: "Unauthorized" })
+        return;
+    }
+    else {
+        try {
+            const course = await Course.findOne({ courseCode: courseCode });
+            if (!course) {
+                res.status(404).json({ message: "Course not found" })
+            }
+            await Course.deleteOne({ courseCode: req.body.courseCode, courseName: req.body.courseName });
+            res.status(200).json({ message: "Course Deleted" })
+        }
+        catch (error) {
+            res.status(404).json({ message: "Course not found" })
+        }
     }
 }
 
