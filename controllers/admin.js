9160ca7d3788c4e8d1180d506a89b5e61ebdbeda
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-07 21:58:11 +0530

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,58 +1,109 @@
-const { ADMIN_PASSWORD } = require('../config/dotenv');
+const { encryptPassword, checkPassword } = require("../utils/encrypt-decrypt")
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const Course = require('../models/Course');
+const Admin = require('../models/Admin');
+const { JWT_SECRET_KEY_ADMIN } = require('../config/dotenv');
 
-const login = async (req, res) => {
-    const { password } = req.body;
-    if (password === ADMIN_PASSWORD) {
-        const token = await jwt.sign({ password }, process.env.JWT_SECRET, { expiresIn: '1h' })
-        res.status(200).json({ token })
-    }
-    else {
-        res.status(401).json({ message: "Invalid Password" })
-    }
-}
-
-const createCourse = async (req, res) => {
-    const { courseName, courseCode, capacity } = req.body;
-    if (!courseName || !courseCode || !capacity) {
+const signup = async (req, res) => {
+    const { id, password, role, name } = req.body;
+    if (!id || !password || !role || !name) {
         res.status(400).json({ message: "Invalid Input" })
         return;
     }
     try {
-        const response = await Course.create({
-            courseName,
-            courseCode,
-            capacity: parseInt(capacity),
+        password_hash = encryptPassword(password)
+        const response = await Admin.create({
+            admin_id: id,
+            password: password_hash,
+            role: role,
+            name: name
         });
-        res.status(200).json({ message: "Course Created" })
+        res.status(200).json({ message: "Admin Created" })
     }
     catch (error) {
-        res.status(400).json({ message: "Course already exists" });
+        res.status(400).json({ message: "Admin already exists" });
+    }
+}
+
+const login = async (req, res) => {
+    const { id, password, role } = req.body
+    if (!id || !password || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
+    try {
+        const admin = await Admin.findOne({ admin_id: id });
+        if (!admin || admin.role !== role) {
+            res.status(404).json({ message: "Admin not found" })
+            return;
+        }
+        const flag = checkPassword(String(admin.password), String(password))
+        if (flag) {
+            const token = jwt.sign({ id, role }, JWT_SECRET_KEY_ADMIN, { expiresIn: '1h' });
+            res.status(200).json({ message: "Admin Logged In", token })
+        }
+        else {
+            res.status(401).json({ message: "Invalid Password" })
+        }
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const createCourse = async (req, res) => {
+    const { courseName, courseCode, capacity, role } = req.body;
+    if (!courseName || !courseCode || !capacity || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
+    if (role !== "admin") {
+        res.status(401).json({ message: "Unauthorized" })
+        return;
+    }
+    else {
+        try {
+            const response = await Course.create({
+                courseName,
+                courseCode,
+                capacity: parseInt(capacity),
+            });
+            res.status(200).json({ message: "Course Created" })
+        }
+        catch (error) {
+            res.status(400).json({ message: "Course already exists" });
+        }
     }
 }
 
 const deleteCourse = async (req, res) => {
-    try {
-        console.log(req.body);
-        const course = await Course.findOne({
-            courseCode: req.body.courseCode,
-            courseName: req.body.courseName
-        });
-        console.log(course);
-        if (!course) {
-            res.status(404).json({ message: error.message })
-        }
-        await Course.deleteOne({ courseCode: req.body.courseCode, courseName: req.body.courseName });
-        res.status(200).json({ message: "Course Deleted" })
+    const { courseCode, courseName, role } = req.body;
+    if (!courseCode || !courseName || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
     }
-    catch (error) {
-        res.status(500).json({ message: "Internal Server Error" })
+    if (role !== "admin") {
+        res.status(401).json({ message: "Unauthorized" })
+        return;
+    }
+    else {
+        try {
+            const course = await Course.findOne({ courseCode: courseCode });
+            if (!course) {
+                res.status(404).json({ message: error.message })
+            }
+            await Course.deleteOne({ courseCode: req.body.courseCode, courseName: req.body.courseName });
+            res.status(200).json({ message: "Course Deleted" })
+        }
+        catch (error) {
+            res.status(500).json({ message: "Internal Server Error" })
+        }
     }
 }
 
 module.exports = {
+    signup,
     login,
     createCourse,
     deleteCourse
