a3bd21c45d59f542978c9188c20a305b33eb65d4
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-07 21:58:27 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,17 @@
 const express = require("express")
 const router = express.Router();
 const {
+    signup,
     login,
     createCourse,
     deleteCourse
 } = require('../controllers/admin')
-const verifyToken = require('../middlewares/verify')
+const { verifyTokenAdmin } = require('../middlewares/verify')
 
-
+router.post('/signup', signup)
 router.post('/login', login)
-router.post('/courses', verifyToken, createCourse)
+router.post('/courses', verifyTokenAdmin, createCourse)
 
-router.delete('/courses', verifyToken, deleteCourse);
+router.delete('/courses', verifyTokenAdmin, deleteCourse);
 
 module.exports = router;
