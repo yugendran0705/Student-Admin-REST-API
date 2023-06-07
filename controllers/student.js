9370e93a2d50032b721c3892cb934c6e41d52e28
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-07 22:51:28 +0530

    Update student.js

--- controllers/student.js
+++ controllers/student.js
@@ -5,130 +5,105 @@ const { encryptPassword, checkPassword } = require("../utils/encrypt-decrypt")
 
 
 const signUp = async (req, res) => {
+    const { register_number, name, password, courses, role } = req.body;
+    if (!register_number || !name || !password || !courses || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
+    course_list = String(courses).split(",")
+    password_hash = encryptPassword(password)
+    const courses_check = await Course.find({ courseName: { $in: course_list } })
+    if (courses_check.length !== course_list.length) {
+        res.status(400).json({ message: "Invalid Course" })
+        return;
+    }
     try {
-        const { register_number, name, password, courses, role } = req.body;
-        if (!register_number || !name || !password || !courses || !role) {
-            res.status(400).json({ message: "Invalid Input" })
-            return;
-        }
-        course_list = String(courses).split(",")
-        password_hash = encryptPassword(password)
-        const courses_check = await Course.find({ courseName: { $in: course_list } })
-        if (courses_check.length !== course_list.length) {
-            res.status(400).json({ message: "Invalid Course" })
-            return;
-        }
-        try {
-            const response = await Student.create({
-                regno: register_number,
-                name: name,
-                password: password_hash,
-                courses: course_list,
-                role: role
-            })
-            res.status(200).json({ message: "Student Created" })
-        }
-        catch (error) {
-            res.status(409).json({ message: "Student already exists" })
-        }
+        const response = await Student.create({
+            regno: register_number,
+            name: name,
+            password: password_hash,
+            courses: course_list,
+            role: role
+        })
+        res.status(200).json({ message: "Student Created" })
     }
     catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
+        res.status(409).json({ message: "Student already exists" })
     }
 }
 
 const login = async (req, res) => {
-    try {
-        const { register_number, password, role } = req.body;
-        if (!register_number || !password || !role) {
-            res.status(400).json({ message: "Invalid Input" })
-            return;
-        }
-        const student = await Student.findOne({ regno: register_number })
+    const { register_number, password, role } = req.body;
+    if (!register_number || !password || !role) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
+    const student = await Student.findOne({ regno: register_number })
 
-        if (!student || student.role !== "student") {
-            res.status(401).json({ message: "Invalid Register Number" })
+    if (!student || student.role !== "student") {
+        res.status(401).json({ message: "Invalid Register Number" })
+    }
+    else {
+        const flag = checkPassword(String(student.password), String(password))
+        if (flag) {
+            const token = jwt.sign({ register_number, role }, process.env.JWT_SECRET_KEY_STUDENT, { expiresIn: '1h' })
+            res.status(200).json({ token })
         }
         else {
-            const flag = checkPassword(String(student.password), String(password))
-            if (flag) {
-                const token = jwt.sign({ register_number, role }, process.env.JWT_SECRET_KEY_STUDENT, { expiresIn: '1h' })
-                res.status(200).json({ token })
-            }
-            else {
-                res.status(401).json({ message: "Invalid Password" })
-            }
+            res.status(401).json({ message: "Invalid Password" })
         }
-    }
-    catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
     }
 }
 const registerCourse = async (req, res) => {
-    try {
-        const { course_id, register_number } = req.body;
-        if (!course_id || !register_number) {
-            res.status(400).json({ message: "Invalid Input" })
+    const { course_id, register_number } = req.body;
+    if (!course_id || !register_number) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
+    const course = await Course.findOne({ courseCode: course_id })
+    if (!course) {
+        res.status(401).json({ message: "Invalid Course ID" })
+        return;
+    }
+    else {
+        const student = await Student.findOne({ regno: register_number })
+        if (!student || student.role !== "student") {
+            res.status(401).json({ message: "Invalid Register Number" })
             return;
         }
-        const course = await Course.findOne({ courseCode: course_id })
-        if (!course) {
-            res.status(401).json({ message: "Invalid Course ID" })
-            return;
+        course_name = course.courseName
+        if (student.courses.includes(course_name)) {
+            res.status(409).json({ message: "Course already registered" })
         }
         else {
-            const student = await Student.findOne({ regno: register_number })
-            if (!student || student.role !== "student") {
-                res.status(401).json({ message: "Invalid Register Number" })
-                return;
-            }
-            course_name = course.courseName
-            if (student.courses.includes(course_name)) {
-                res.status(409).json({ message: "Course already registered" })
-            }
-            else {
-                student.courses.push(course_name)
-                await student.save()
-                res.status(200).json({ message: "Course Registered" })
-            }
+            student.courses.push(course_name)
+            await student.save()
+            res.status(200).json({ message: "Course Registered" })
         }
-    }
-    catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
     }
 }
 
 const listCourses = async (req, res) => {
-    try {
-        const courses = await Course.find()
-        if (courses.length === 0) {
-            res.status(400).json({ message: "No Courses" })
-            return;
-        }
-        res.status(200).json({ courses })
+    const courses = await Course.find()
+    if (courses.length === 0) {
+        res.status(400).json({ message: "No Courses" })
+        return;
     }
-    catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
-    }
+    res.status(200).json({ courses })
 }
 
 const viewRegisteredCourses = async (req, res) => {
+    const { register_number } = req.body;
+    if (!register_number) {
+        res.status(400).json({ message: "Invalid Input" })
+        return;
+    }
     try {
-        const { register_number } = req.body;
-        if (!register_number) {
-            res.status(400).json({ message: "Invalid Input" })
-            return;
-        }
-        try {
-            const student = await Student.findOne({ regno: register_number }).populate('courses')  // populate() is used to get the details of the courses from the course_id
-            res.status(200).json({ courses: student.courses })
-        }
-        catch (error) {
-            res.status(400).json({ message: "Invalid Register Number" })
-        }
+        const student = await Student.findOne({ regno: register_number }).populate('courses')  // populate() is used to get the details of the courses from the course_id
+        res.status(200).json({ courses: student.courses })
     }
     catch (error) {
-        res.status(error.statusCode).json({ message: error.message })
+        res.status(400).json({ message: "Invalid Register Number" })
     }
 }
 
